21111f70069e7fab869ef083ea0b17c7deed2abc
MarinaSigida <sigida.web@gmail.com>
2023-06-07 02:24:54 +0200

    PoC. initial implementation

--- src/index.js
+++ src/index.js
@@ -7,18 +7,8 @@ const refs = {
   catInfo: document.querySelector('div.cat-info'),
 };
 
-
-// fetchBreeds()
-// .then(renderCatCard)
-// .catch(onFetchError);
-
-// function renderCatCard(breed)
-
-// function onFetchError (error){
-//     alert ('ERROR ERROR ERROR');
-// }
-
 const url = 'https://api.thecatapi.com/v1/breeds';
+const catInfoUrl = 'https://api.thecatapi.com/v1/images/search?breed_ids=';
 const options = {
   headers: {
     'x-api-key':
@@ -26,36 +16,73 @@ const options = {
   },
 };
 
-fetch(url, options).then(response => {
-  response.json().then(renderBreedSelector);
-});
+hide(refs.breedSelect);
+hide(refs.error);
+show(refs.loader);
+
+fetch(url, options)
+  .then(response => {
+    response.json().then(renderBreedSelector);
+    hide(refs.loader);
+  })
+  .catch(() => {
+    show(refs.error);
+    hide(refs.loader);
+  });
 
 function renderBreedSelector(breedList) {
-  let breedOptions = breedList.map(({ id, name}) => {
-      return `
+  let breedOptions = breedList.map(({ id, name }) => {
+    return `
       <select class="breed-select">
       <option value="${id}">${name}</option>
       </select>
       `;
-    });
-    refs.breedSelect.innerHTML = breedOptions.join("");
+  });
+  refs.breedSelect.innerHTML = breedOptions.join('');
+  show(refs.breedSelect);
 }
 
-// let breedOptions = breedList.map(({ id, name, image, description, temperament }) => {
-//     return `
-//     <select class="breed-select">
-//     <option value="${id}">"${name}"</option>
-//     </select>
-//     `;
-//   });
+function hide(el) {
+  el.style.display = 'none';
+}
 
-//   refs.breedSelector.insertAdjacentHTML("beforeend", breedOptions.join(""));
+function show(el) {
+  el.style.display = 'block';
+}
 
-// function fetchBreeds(breedId) {
-//   return fetch(url, options).then(
-//     response => {
-//       return response.json()
-//       .then (console.log)
-//     }
-//   );
-// }
+function fetchCatByBreed(breedId) {
+  return fetch(catInfoUrl + breedId, options);
+}
+
+function renderCatInfo(cats) {
+  let catInfo = cats.map(({ breeds, url }) => {
+    return `
+        <div class="cat-info">
+        <img src="${url}" alt="${breeds[0].name}" width="600"/>
+        <h1>${breeds[0].name}</h1>
+        <p>${breeds[0].description}</p>
+        <p><span>Temperament:</span>
+        ${breeds[0].temperament}</p>
+        </div>
+          `;
+  });
+  refs.catInfo.innerHTML = catInfo.join('');
+  show(refs.catInfo);
+}
+
+refs.breedSelect.addEventListener('change', event => {
+  hide(refs.error);
+  hide(refs.catInfo);
+  show(refs.loader);
+  fetchCatByBreed(event.target.value).then(response => {
+    response
+      .json()
+      .then(renderCatInfo)
+      .catch(() => {
+        show(refs.error);
+        hide(refs.loader);
+        hide(refs.catInfo);
+      });
+    hide(refs.loader);
+  });
+});
